5543fd7c6566ca2cda87377d83ea3b4b6b96683e
kitae0522 <kitae040522@gmail.com>
2023-07-26 03:04:28 +0900

    FIX: 2일차 과제 Solution 코드 수정
    1번 과제에서 슬라이싱 구간을 수정했습니다.

--- Day2/HW01.py
+++ Day2/HW01.py
@@ -2,5 +2,5 @@ name = input("당신의 이름은 무엇인가요? : ")
 age = int(input(f"{name}님 반가워요! 나이는 어떻게 되세요? : "))
 _ = input(f"이제 나이가 바뀌어서 {age - 2}살이거나 {age - 1}살이시겠네요. : ")
 school = input("그렇다면, 현재 어디 학교에 다니고 계신가요? : ")
-school = school[0:school.find("교")]
+school = school[0:school.find("교")+1]
 print(f"{school}에 재학 중이시군요. 좋은 하루 보내세요!")
