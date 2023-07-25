e42be8a80c453f75283941e38f965e6f118d17c2
kitae0522 <kitae040522@gmail.com>
2023-07-26 02:56:14 +0900

    FEAT: 2일차 과제 Solution 코드 추가
    2일차 필수 과제, 선택 과제의 정답 코드를 추가했습니다.

---
+++ Day2/HW03.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+	def main():
+		# 💩
+		item = 3
+		str(type(item)) == "<class 'int'>" and print(1)
+		str(type(item)) == "<class 'float'>" and print(2)
+		str(type(item)) == "<class 'str'>" and print(3)
+
+		# isinstance()를 사용해보는 것은 어떨까?
+		# isinstance($[ele], $[data_type]) -> bool
+		# $[ele]의 자료형이 $[data_type]과 같다면 True를, 아니면 False를 리턴한다.
+		
+		# ✅
+		item = 3
+		isinstance(item, int) and print(1)
+		isinstance(item, float) and print(2)
+		isinstance(item, str) and print(3)
+	main()
