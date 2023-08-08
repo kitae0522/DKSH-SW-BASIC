db744d573f9a371d88be6fe23b569b8d22698438
kitae0522 <kitae040522@gmail.com>
2023-08-08 23:32:13 +0900

    STYLE: Day3 파이썬 파일 코드 포맷팅
    3일차 파이썬 파일들 모두 코드 포맷팅을 진행했습니다.

--- Day3/HW04.py
+++ Day3/HW04.py
@@ -1,26 +1,28 @@
 import random
 
 if __name__ == '__main__':
-	def main():
-		K = random.randint(1, 10)
-		if K == 1:
-			print("맥도날드 빅맥 세트")
-		elif K == 2:
-			print("맥도날드 맥크리스피 디럭스 버거 세트")
-		elif K == 3:
-			print("맥도날드 맥스파이시 상하이 버거 세트")
-		elif K == 4:
-			print("맥도날드 1955 버거 세트")
-		elif K == 5:
-			print("맥도날드 에그 불고기 버거 세트")
-		elif K == 6:
-			print("맥도날드 베이컨 토마토 디럭스 세트")
-		elif K == 7:
-			print("맥도날드 슈슈 버거 세트")
-		elif K == 8:
-			print("맥도날드 슈비 버거 세트")
-		elif K == 9:
-			print("맥도날드 맥크리스피 클래 버거 세트")
-		elif K == 10:
-			print("맥도날드 쿼터파운더 치즈 세트")
-	main()
+    def main():
+        K = random.randint(1, 10)
+        if K == 1:
+            print("맥도날드 빅맥 세트")
+        elif K == 2:
+            print("맥도날드 맥크리스피 디럭스 버거 세트")
+        elif K == 3:
+            print("맥도날드 맥스파이시 상하이 버거 세트")
+        elif K == 4:
+            print("맥도날드 1955 버거 세트")
+        elif K == 5:
+            print("맥도날드 에그 불고기 버거 세트")
+        elif K == 6:
+            print("맥도날드 베이컨 토마토 디럭스 세트")
+        elif K == 7:
+            print("맥도날드 슈슈 버거 세트")
+        elif K == 8:
+            print("맥도날드 슈비 버거 세트")
+        elif K == 9:
+            print("맥도날드 맥크리스피 클래 버거 세트")
+        elif K == 10:
+            print("맥도날드 쿼터파운더 치즈 세트")
+
+
+    main()
