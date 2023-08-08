27b791411e11864099d5c1ccdbfc26120fddabf0
kitae0522 <kitae040522@gmail.com>
2023-08-08 23:24:50 +0900

    FEAT: 4일차 과제 Solution 코드 추가
    4일차 필수 과제, 선택 과제의 정답 코드를 추가했습니다.

---
+++ Day4/HW06.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+	def main():
+		x = int(input())
+		for i in range(x + 1):
+		    for j in range(x - i):
+		        print("*", end="")
+		    print()
+	main()
---
+++ Day4/HW08.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+	def main():
+		T = int(input())
+		S = 0
+		for i in range(T):
+			N = int(input())
+			if N:
+				S += 1
+			else:
+				S -= 1
+		if S > 0:
+			print("Junhee is cute!")
+		else:
+			print("Junhee is not cute!")
+	main()
